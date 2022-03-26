3064ece02c18fe6c4e494b281e46f80917e13224
Nick Holmstedt <holmstn@gmail.com>
2022-03-25 20:14:30 -0400

    bootstrap

---
+++ components/landing.tsx
@@ -0,0 +1,15 @@
+export const Landing = () => {
+  return (
+    <div className="flex py-12 align-center justify-center items-center mx-auto">
+      <div className="bg-slate-600 mx-12 py-24 w-1/4 rounded">
+        <span className="ml-12">Find Team</span>
+      </div>
+      <div className="bg-slate-600 mx-12 py-24 w-1/4 rounded">
+        <span className="ml-12">Create Team</span>
+      </div>
+      <div className="bg-slate-600 mx-12 py-24 w-1/4 rounded">
+        <span className="ml-12">Stats</span>
+      </div>
+    </div>
+  );
+};
---
+++ docs/pages/landing.md
@@ -0,0 +1,10 @@
+Initial landing page.
+
+ACTIONS:
+
+- Login
+- Find Team
+- Create Team
+- Game Statistics
+
+Mockup:
